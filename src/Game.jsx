2615b44d86a41a405c355913e3bec1a2b060c6c7
Student <example@example.com>
2019-09-19 08:55:43 -0700

    Passing Data Through Props

--- src/Game.jsx
+++ src/Game.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+
+
 class Square extends React.Component {
   render() {
     return (
       <button className="square">
-        {/* TODO */}
+        {this.props.value}
       </button>
     );
   }
@@ -11,7 +13,7 @@ class Square extends React.Component {
 
 class Board extends React.Component {
   renderSquare(i) {
-    return <Square />;
+    return <Square value={i}/>;
   }
 
   render() {
