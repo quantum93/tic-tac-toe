10fe8a28ada22f69819835a54bf933a51f38a001
Student <example@example.com>
2019-09-19 09:24:31 -0700

    make Square component function component

--- src/Game.jsx
+++ src/Game.jsx
@@ -1,22 +1,14 @@
 import React from 'react';
 
 
-class Square extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      value: null,
-    };
-  }
-
-  render() {
+function Square(props){
     return (
-      <button className="square" onClick={() => this.props.onClick()}>
-        {this.props.value}
+      <button className="square" onClick={props.onClick}>
+        {props.value}
       </button>
     );
   }
-}
+
 
 class Board extends React.Component {
   constructor(props){
